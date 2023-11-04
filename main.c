1169c62098b02edb72b4f43531d6894b7d511a0b
ekajdfk <yoosy320@gmail.com>
2023-11-04 21:26:30 +0900

    3-2

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 int main(int argc, char *argv[])
 {
   int i;
-  int grade[SIZE];
-  int score[SIZE];
+  int a[SIZE] = {1, 2, 3, 4, 5};
+  int b[SIZE] = {1, 2, 3, 4, 4};
+  int flag = 0;
   
   for(i=0; i<SIZE; i++)
-    grade[i] = rand()%100 + 1;
-    
-  for(i=0; i<SIZE; i++)
-    score[i] = grade[i];
+  {
+           if(a[i] != b[i])
+           {
+             printf("array a and b are not the same\n");
+             flag = 1;
+           }  
+  }
   
-  for(i=0; i<SIZE; i++)
-    printf("score[%d] = %d (grade:%i)\n", i, score[i], grade[i]);
+  if (flag == 0)
+     printf("array a and b are the same\n");
   
   system("PAUSE");	
   return 0;
